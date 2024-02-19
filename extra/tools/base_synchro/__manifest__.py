1cf1975b5b70280c229d47c9342870bb309acc25
avalos101 <104409303+avalos101@users.noreply.github.com>
2024-02-19 13:23:02 -0500

    Multi DB syncro

---
+++ extra/tools/base_synchro/__manifest__.py
@@ -0,0 +1,25 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Multi-DB Synchronization",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "summary": "Multi-DB Synchronization",
+    "author": "inCore",
+    "website": "incore.app",
+    "maintainer": "inCore",
+    "images": [
+        "static/description/Synchro.png",
+    ],
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/base_synchro_view.xml",
+        "views/base_synchro_view.xml",
+        "views/res_request_view.xml",
+    ],
+    "installable": True,
+}
